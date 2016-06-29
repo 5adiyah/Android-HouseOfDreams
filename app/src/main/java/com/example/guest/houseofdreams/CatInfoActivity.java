1f957d2fe05cd97eb2cd41388f446816764770db
Kendra Dunham and Sadio Ali <kendra.dunham@gmail.com>
2016-06-29 16:16:13 -0700

    Add cute cat pics

--- app/src/main/java/com/example/guest/houseofdreams/CatInfoActivity.java
+++ app/src/main/java/com/example/guest/houseofdreams/CatInfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -12,6 +13,7 @@ import butterknife.ButterKnife;
 public class CatInfoActivity extends AppCompatActivity {
     private static final String TAG = "--------";
     @Bind(R.id.showCat) TextView mShowCat;
+    @Bind(R.id.catPicture) ImageView mCatPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,40 +21,54 @@ public class CatInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cat_info);
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
-//        Integer intId = Integer.parseInt(id);
-//        Log.v(TAG, id);
+//
         ButterKnife.bind(this);
         Log.i(TAG, id);
             if(id.equals("2130837582")) {
-                mShowCat.setText("this is cat1");
+                mShowCat.setText("This is Benita.");
+                mCatPicture.setImageResource(R.drawable.cat1);
             } else if(id.equals("2130837580")){
-                mShowCat.setText("biscuit");
+                mShowCat.setText("This is Biscuit");
+                mCatPicture.setImageResource(R.drawable.biscuit);
             } else if(id.equals("2130837581")){ //change this
-                mShowCat.setText("buttons");
+                mShowCat.setText("This is Buttons");
+                mCatPicture.setImageResource(R.drawable.buttons);
             } else if(id.equals("2130837583")){
-                mShowCat.setText("cypress");
+                mShowCat.setText("This is Cypress");
+                mCatPicture.setImageResource(R.drawable.cypress);
             } else if(id.equals("2130837584")){
-                mShowCat.setText("hadley");
+                mShowCat.setText("This is Hadley");
+                mCatPicture.setImageResource(R.drawable.hadley);
             } else if(id.equals("2130837585")){
-                mShowCat.setText("ian");
+                mShowCat.setText("This is Ian");
+                mCatPicture.setImageResource(R.drawable.ian);
             } else if(id.equals("2130837591")) { //does this keep changing?
-                mShowCat.setText("raven");
+                mShowCat.setText("This is Raven");
+                mCatPicture.setImageResource(R.drawable.raven);
             } else if(id.equals("2130837586")){
-                mShowCat.setText("inez");
+                mShowCat.setText("This is Inez");
+                mCatPicture.setImageResource(R.drawable.inez);
             } else if(id.equals("2130837587")){
-                mShowCat.setText("linus");
+                mShowCat.setText("This is Linus");
+                mCatPicture.setImageResource(R.drawable.linus);
             } else if(id.equals("2130837588")){
-                mShowCat.setText("max");
+                mShowCat.setText("This is Max");
+                mCatPicture.setImageResource(R.drawable.max);
             } else if(id.equals("2130837589")){
-                mShowCat.setText("mittens");
+                mShowCat.setText("This is Mittens");
+                mCatPicture.setImageResource(R.drawable.mittens);
             } else if(id.equals("2130837590")){
-                mShowCat.setText("rainbow");
+                mShowCat.setText("This is Rainbow");
+                mCatPicture.setImageResource(R.drawable.rainbow);
             } else if(id.equals("2130837592")){
-                mShowCat.setText("ruby");
+                mShowCat.setText("This is Ruby");
+                mCatPicture.setImageResource(R.drawable.ruby);
             } else if(id.equals("2130837593")){
-                mShowCat.setText("stella");
+                mShowCat.setText("This is Stella");
+                mCatPicture.setImageResource(R.drawable.stella);
             } else if(id.equals("2130837594")){
-                mShowCat.setText("taffy");
+                mShowCat.setText("This is Taffy");
+                mCatPicture.setImageResource(R.drawable.taffy);
             } else {
                 mShowCat.setText("you have failed this and everything else in life");
             }
